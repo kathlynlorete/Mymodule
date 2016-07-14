5288731885f0da83afc61c65a84957e1ec88e485
kathlynlorete <ethylene_kj22@yahoo.com>
2016-07-14 19:15:45 +0800

    hello

---
+++ app.js
@@ -0,0 +1,11 @@
+const util = require('./Mymodule');
+
+var area = util.computeArea(10,50);
+
+console.log("Area is :" + area);
+
+var perimeter =
+util.computePerimeter(10,50);
+
+console.log("Perimeter is :"
++ perimeter);
